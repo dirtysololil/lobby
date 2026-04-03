55f9c1cc1832136564ac38f3c6b88165d62272b2
root <root@2654793-vq24688.twc1.net>
2026-04-03 07:06:25 +0300

    Исправления

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,18 +15,14 @@ module.exports = {
     },
     {
       name: "lobby-web",
-      cwd: "apps/web",
+      cwd: "/var/www/www-root/data/www/lobby",
       script: "node",
-      args: "./node_modules/next/dist/bin/next start --hostname 127.0.0.1 --port 3000",
-      interpreter: "none",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "512M",
+      args: "apps/web/.next/standalone/apps/web/server.js",
       env: {
         NODE_ENV: "production",
         HOSTNAME: "127.0.0.1",
-        PORT: "3000",
-      },
+        PORT: "3000"
+      }
     },
     {
       name: "lobby-worker",
