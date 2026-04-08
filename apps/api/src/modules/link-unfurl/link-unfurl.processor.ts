8266d8c3539178d741330b7ccd1237b48259aab1
dirtysololil <chyreevofficial@gmail.com>
2026-04-08 14:31:16 +0500

    Обновил Readme

--- apps/api/src/modules/link-unfurl/link-unfurl.processor.ts
+++ apps/api/src/modules/link-unfurl/link-unfurl.processor.ts
@@ -1,4 +1,5 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
+import { Logger } from '@nestjs/common';
 import type { Job } from 'bullmq';
 import { DirectMessagesService } from '../direct-messages/direct-messages.service';
 import { DM_LINK_UNFURL_QUEUE } from '../queue/queue.constants';
@@ -6,6 +7,8 @@ import { LinkUnfurlService } from './link-unfurl.service';
 
 @Processor(DM_LINK_UNFURL_QUEUE)
 export class LinkUnfurlProcessor extends WorkerHost {
+  private readonly logger = new Logger(LinkUnfurlProcessor.name);
+
   public constructor(
     private readonly linkUnfurlService: LinkUnfurlService,
     private readonly directMessagesService: DirectMessagesService,
@@ -18,6 +21,8 @@ export class LinkUnfurlProcessor extends WorkerHost {
       return;
     }
 
+    this.logger.log(`Processing DM link unfurl for message ${job.data.messageId}`);
+
     const didChange = await this.linkUnfurlService.processMessage(
       job.data.messageId,
     );
@@ -25,6 +30,9 @@ export class LinkUnfurlProcessor extends WorkerHost {
     if (didChange) {
       await this.directMessagesService.emitMessageUpdatedSignal(
         job.data.messageId,
+      );
+      this.logger.log(
+        `Completed DM link unfurl for message ${job.data.messageId}`,
       );
     }
   }
